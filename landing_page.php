822d68e0ad34cc4224cf6d54bf6d4e30bfb86947
Ash Singh <Ash@Ashs-MBP.home>
2015-08-18 20:44:43 +0100

    new files added

---
+++ landing_page.php
@@ -0,0 +1,86 @@
+<?php 
+/*
+	Template Name: Landing Page
+*/
+
+ ?>
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Ten
+ * @since Twenty Ten 1.0
+ */
+
+get_header(); ?>
+
+		<div id="container">
+			<div id="content" role="main">
+
+			<?php
+			/* Run the loop to output the page.
+			 * If you want to overload this in a child theme then include a file
+			 * called loop-page.php and that will be used instead.
+			 */
+			get_template_part( 'loop', 'page' );
+		
+		?>
+
+			</div><!-- #content -->
+		</div><!-- #container -->
+		<div id="boxes">
+			<div class="box">
+				<h2><a href="http://www.whitelogicsolutions.com/white-logic-services/web-development/">Website Design & Development</a></h2>
+				<p class="sub_t">Professional website designing & development</p>
+				<img src="wp-content/uploads/2011/07/img1.png" alt="Website Building" title="Get cheap Web design" />
+				<p>Give your business a professional and flawless website design with full technical support with bespoke development. In Competitive rate get premium service.<a href="http://www.whitelogicsolutions.com/white-logic-services/web-development/"><span class="read">read more</span></a></p>
+			</div>
+			<div class="box">
+				<h2><a href="http://www.whitelogicsolutions.com/white-logic-services/web-development#ecommerce">E-Commerce</a></h2>
+				<p class="sub_t">Get web presence, have ecommerce shop</p>
+				<img src="wp-content/uploads/2011/07/img2.png" alt="Ecommerce website" title="Get you own online business" />
+				<p>Increase revenue by having an online shop, which gives your business global face and grow your business. We'll be catalyst for your business. Grab the easy profit here. <a href="http://www.whitelogicsolutions.com/white-logic-services/web-development#ecommerce"><span class="read">read more</span></a></p>
+			</div>
+			<div class="box">
+				<h2><a href="http://www.whitelogicsolutions.com/white-logic-services/search-engine-optimisation/">SEO & Internet Marketing</a></h2>
+				<p class="sub_t">Put forward yourself to the world</p>
+				<img src="wp-content/uploads/2011/07/img3.png" alt="SEO and Marketing" title="Pump your profit with WLS SEO services" />
+				<p>Web presence with accountability, so your focused customer can find your business easily. Market yourself by WLS, and observe the difference between the success and achievements.<a href="http://www.whitelogicsolutions.com/white-logic-services/search-engine-optimisation/"><span class="read">read more</span></a></p>
+			</div>
+		</div>
+		
+<?php get_sidebar(); ?>
+
+<div id="featured_box">
+	<h2 class="featured">Featured Web Designs</h2>
+		<ul>
+			<li><a href=""><img src="wp-content/uploads/2011/07/template1.png" alt="" title="" /><p>Template name</p></a></li>
+			<li><a href=""><img src="wp-content/uploads/2011/07/template2.png" alt="" title="" /><p>Template name</p></a></li>
+			<li><a href=""><img src="wp-content/uploads/2011/07/template3.png" alt="" title="" /><p>Template name</p></a></li>
+		</ul>
+		<h3 class="feat">Web Design Services</h3>
+		<p class="feat_p">All our web design and ecommerce designs are entirely custom built to your specific needs, no templates used. Highly skilled web designers will create your company an impressive and effective online presence. Our website design aftercare service will ensure your website&#39;s stability and maintain your critical internet services.</p>
+</div>
+<div id="testimonial">
+	<h3 class="feat">Testimonal</h3>
+			<img src="wp-content/uploads/2011/07/testimonial.png" alt="" title="" />
+  		<p>Search Engine Optimimisation (SEO) is a specialized subject. It is also not without risk in choosing which SEO company to place your cash and trust in, unfortunately 'SEO Experts' are now the new utilities cold call sellers where more often the same sales pitch is put forward with little content to attribute to it. We at Henri Beene have been very fortunate in having a cold call from Antony <a href=""><span class="read">read more</span></a> </p>  
+
+</div>
+<div class="hreview-aggregate">
+   <h3 class="item">
+      <span class="fn">White Logic Solutions</span>
+   </h3>
+   <span class="summary">White Logic Solutions provides customised internet solutions for businesses. In terms of SEO, PPC, Internet Marketing etc.</span>
+   <span><span class="rating">4.5</span> out of 5 based on <span class="votes">29</span> ratings.</span>
+   <span><span class="count">12</span> recent customer reviews</span>
+</div>
+<br/>
+<?php get_footer(); ?>
+			<div style="width:100%;margin:auto; text-align:center;padding-bottom:5px;"><a style="color:#666;text-decoration:none;" href="http://www.opencart.com/index.php?route=extension/extension&filter_username=whitelogicsolutions">Opencart Extensions</a>&nbsp; | &nbsp;<a style="color:#666;text-decoration:none;" href="http://www.opencart.com/index.php?route=extension/extension/info&extension_id=2049">Image showcase opencart 1.4.x, 1.5.x extension</a>&nbsp; | &nbsp; <a style="color:#666;text-decoration:none;" href="http://www.opencart.com/index.php?route=extension/extension/info&extension_id=1820">Banner Image rotator opencart 1.4.x extension</a> &nbsp; | &nbsp; <a style="color:#666;text-decoration:none;"  href="http://www.whitelogicsolutions.com">Logic Solution Company</a></div>
